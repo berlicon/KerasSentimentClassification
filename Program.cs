b7bc150b4531aad173f1a084b8e722677363b9ec
berlicon <berlicon@mail.ru>
2020-01-21 00:20:57 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KerasSentimentClassification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SentimentClassification.Run();
+            SentimentClassification.Predict("I hate you");
+            SentimentClassification.Predict("I care about you");
+
+            Console.ReadLine();
+        }
+    }
+}
